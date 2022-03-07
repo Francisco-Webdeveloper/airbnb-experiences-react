1825faddc491743a8b56ea7bcb0ff3e6c632791d
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-03-07 14:06:38 +0100

    rendered <Route /> to the DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./components/Router";
 import "./style.css";
-import App from "./App";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
